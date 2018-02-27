41f3f0145cd4b3017e62515aea012ad0e3d26b90
psycho909 <psychosocial909@gmail.com>
2018-02-27 14:43:19 +0800

    123

---
+++ project02/01.project/src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">
+        <h3>{{message}}</h3>
+        <input type="radio" v-model="selected" value="Item1"><label>Item1</label>
+        <input type="radio" v-model="selected" value="Item2"><label>Item2</label>
+        <input type="radio" v-model="selected" value="Item3"><label>Item3</label>
+        <div>selected = {{ selected }}</div>
+    </div>
+</template>
+<script>
+    export default {
+        name:'Hello',
+        data() {
+            return {
+                message:'Hello World',
+                selected:""
+            }
+        }
+    }
+</script>
